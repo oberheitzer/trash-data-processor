ad69b0b998158f8ee4f5f2f0193d7e385229f034
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-03 14:01:07 +0200

    Create Communicator, Validator, Handler and Constant classes to handle user interactions #8

---
+++ TDP.Main/Helpers/Handler.cs
@@ -0,0 +1,30 @@
+﻿namespace TDP.Main.Helpers;
+
+/// <summary>
+/// Handles the interaction during the running of the application.
+/// </summary>
+public static class Handler
+{
+    public static void Handle(int command)
+    {
+        switch (command)
+        {
+            case 1:
+                Update();
+                break;
+            case 2:
+                Reset();
+                break;
+        }
+    }
+
+    private static void Reset()
+    {
+
+    }
+
+    private static void Update()
+    {
+
+    }
+}
--- TDP.Main/Program.cs
+++ TDP.Main/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TDP.Main.Helpers;
+
+Communicator.Introduce();
+Communicator.DisplayMenu();
+
+string? input = Console.ReadLine();
+
+do
+{
+    if (Validator.IsValid(input: input))
+    {
+        if (input != null && input.ToLower() == Constant.MenuCommand.ToLower())
+        {
+            Communicator.DisplayMenu();
+        }
+        else if (input != null && input == Constant.ExitCommand)
+        {
+            continue;
+        }
+        else
+        {
+            if (input != null && input == Constant.ResetCommand)
+            {
+                Communicator.Confirm(message: Constant.DatabaseResetWarning);
+                string? confirmation = Console.ReadLine();
+                if (confirmation != null && confirmation.ToLower() == Constant.Yes.ToLower())
+                {
+                    Handler.Handle(command: int.Parse(input!));
+                }
+                else if (confirmation != null && confirmation.ToLower() == Constant.No.ToLower())
+                {
+                    Communicator.ShowCancelMessage();
+                }
+            }
+            else 
+            {
+                Handler.Handle(command: int.Parse(input!));
+            }
+            Communicator.ShowDoneMessage();
+        }
+    }
+    else
+    {
+        Communicator.ShowInvalidMessage(input: input);
+    }
+    input = Console.ReadLine();
+} while (input != null && input != Constant.ExitCommand);
+
+Communicator.SoLong();
