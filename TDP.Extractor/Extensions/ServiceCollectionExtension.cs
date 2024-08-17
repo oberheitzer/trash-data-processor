71e0890c5693d84835068f60db616f4c75a4e253
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-17 11:22:38 +0200

    Add ServiceCollectionExtension #3

---
+++ TDP.Extractor/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using TDP.Extractor;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICalendarService, CalendarService>();
+        return services;
+    }
+}
