d5d1dfca1548877899b2a5364ecd2475972f217e
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-22 20:39:52 +0200

    Refact constants value in Http project #18

--- TDP.Http/Helpers/Constant.cs
+++ TDP.Http/Helpers/Constant.cs
@@ -4,32 +4,6 @@ public static class Constant
 {
     public static readonly string BaseUrl = "https://vhg.hu";
 
-    public static readonly string Directory = "Calendars";
-
     public static readonly string Folder = "/storage";
 
-    #region Area IV
-    public static readonly string GardonyArea4 = "/1319/Gárdony_IV..pdf";
-    public static readonly string GardonyArea4EmptyPlot = "/1516/Gárdony-IV_üres-telek.pdf";
-    public static readonly string GardonyArea4HolidayHouse = "/1383/Gárdony_IV_üdülő.pdf";
-    #endregion
-
-    #region Area XV
-    public static readonly string GardonyArea15 = "/1319/Gárdony_XV..pdf";
-    public static readonly string GardonyArea15EmptyPlot = "/1516/Gárdony-XV_üres-telek.pdf";
-    public static readonly string GardonyArea15HolidayHouse = "/1383/Gárdony_XV_üdülő.pdf";
-    #endregion
-
-    #region Area XVI
-    public static readonly string GardonyArea16 = "/1319/Gárdony_XVI..pdf";
-    public static readonly string GardonyArea16EmptyPlot = "/1516/Gárdony-XVI_üres-telek.pdf";
-    public static readonly string GardonyArea16HolidayHouse = "/1383/Gárdony_XVI_üdülő.pdf";
-    #endregion
-
-    public static readonly Dictionary<string, string> Areas = new()
-    {
-        { "Gardony_XVI", GardonyArea16 },
-        { "Gardony_XVI_EP", GardonyArea16EmptyPlot },
-        { "Gardony_XVI_HH", GardonyArea16HolidayHouse },
-    };
 }
