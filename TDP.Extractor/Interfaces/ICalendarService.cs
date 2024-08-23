fffb21ae90cd4f9ef173d8bd1493123db1a0212e
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-23 13:16:50 +0200

    Add comments #13

--- TDP.Extractor/Interfaces/ICalendarService.cs
+++ TDP.Extractor/Interfaces/ICalendarService.cs
@@ -7,11 +7,12 @@ public interface ICalendarService
     /// <summary>
     /// Reads the downloaded files.
     /// </summary>
+    /// <param name="file">The downloaded file.</param>
     List<Collection> Read(string file);
 
     /// <summary>
     /// Writes the waste collection days into the specific csv file.
     /// </summary>
-    /// <param name="collections"></param>
+    /// <param name="collections">The list of days when collection happens.</param>
     void Write(List<Collection> collections);
 }
