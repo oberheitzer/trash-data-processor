5218828587a7fe564adb32a7845e2b8401cb5271
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-24 11:38:22 +0200

    Add new constant value for DB communication #23

--- TDP.Http/Helpers/Constant.cs
+++ TDP.Http/Helpers/Constant.cs
@@ -6,4 +6,12 @@ public static class Constant
 
     public static readonly string Folder = "/storage";
 
+    public static readonly string SupabaseUrl = "SUPABASE_URL";
+
+    public static readonly string SupabaseKey = "SUPABASE_KEY";
+
+    public static readonly string ApiKey = "apiKey";
+
+    public static readonly string AreaUri = "areas";
+
 }
