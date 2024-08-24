179b33666c7ca1ede54fa85fd6b7d38712929406
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-24 11:39:42 +0200

    Add the logic of getting areas from DB #23

--- TDP.Http/Interfaces/IDatabaseService.cs
+++ TDP.Http/Interfaces/IDatabaseService.cs
@@ -1,6 +1,12 @@
-﻿namespace TDP.Http.Interfaces;
+﻿using TDP.Domain.Model;
+
+namespace TDP.Http.Interfaces;
 
 public interface IDatabaseService
 {
-
+    /// <summary>
+    /// Gets all areas from database.
+    /// </summary>
+    /// <returns>List of areas.</returns>
+    Task<List<Area>> GetAreasAsync();
 }
