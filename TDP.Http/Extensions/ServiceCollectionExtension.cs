fe973d7472f7c4ac141a08b0c659615bf843eae6
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-24 11:39:10 +0200

    Update ServiceCollectionExtension (DB) #23

--- TDP.Http/Extensions/ServiceCollectionExtension.cs
+++ TDP.Http/Extensions/ServiceCollectionExtension.cs
@@ -8,6 +8,20 @@ public static class ServiceCollectionExtension
 {
     public static ServiceCollection AddServices(this ServiceCollection services)
     {
+        string? supabasUrl = Environment.GetEnvironmentVariable(variable: Constant.SupabaseUrl);
+        string? supabasKey = Environment.GetEnvironmentVariable(variable: Constant.SupabaseKey);
+        if (string.IsNullOrEmpty(supabasUrl) || string.IsNullOrEmpty(supabasKey))
+        {
+            string missingVariable = string.IsNullOrEmpty(supabasUrl) ? Constant.SupabaseUrl : Constant.SupabaseKey;
+            Console.WriteLine($"{missingVariable} is missing from environment variables!");
+        }
+        else
+        {
+            services.AddHttpClient<IDatabaseService, DatabaseService>(httpCLient => {
+                httpCLient.BaseAddress = new Uri(uriString: supabasUrl);
+                httpCLient.DefaultRequestHeaders.Add(name: Constant.ApiKey, value: supabasKey);
+            });
+        }
         services.AddHttpClient<IWasteService, WasteService>(httpClient => {
             httpClient.BaseAddress = new Uri(uriString: Constant.BaseUrl);
         });
