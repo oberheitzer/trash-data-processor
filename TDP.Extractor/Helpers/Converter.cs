baef99d3bead395c53def4c49d1d1d9347d8b3ea
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-25 12:21:06 +0200

    Area converter functions #25

--- TDP.Extractor/Helpers/Converter.cs
+++ TDP.Extractor/Helpers/Converter.cs
@@ -1,9 +1,38 @@
 ﻿using TDP.Domain.Enum;
+using TDP.Domain.Model;
 
 namespace TDP.Extractor.Helpers;
 
 public static class Converter
 {
+    /// <summary>
+    /// Finds and returns the name of the area.
+    /// </summary>
+    /// <param name="line">Title line of the calendar.</param>
+    /// <returns>Name of area.</returns>
+    public static string ToArea(string line)
+    {
+        // TODO more settlement
+        string area = line.Split("Gárdony")[1].Trim();
+        int dotIndex = area.IndexOf('.');
+        if (dotIndex != -1)
+        {
+            return area.Remove(dotIndex);
+        }
+        return area;
+    }
+
+    /// <summary>
+    /// Finds and returns the identifier of the area based of its given name.
+    /// </summary>
+    /// <param name="area">Name of the area.</param>
+    /// <param name="areas">List of areas.</param>
+    /// <returns>The identifier of the area.</returns>
+    public static int ToAreaId(string area, List<Area> areas)
+    // TODO nem lesz jó, mert csak a római számos rész kellene
+    // TODO check zártkert
+        => areas.Single(a => area == a.Name).Id;
+
     public static Property ToProperty(string line) => line.ToLower() switch
     {
         string text when text.Contains(Constant.HolidayHouse) => Property.HolidayHouse,
