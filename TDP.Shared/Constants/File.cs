61c5e845adcca4420fe4e4085a4a854c4350df69
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-08-25 12:16:35 +0200

    More downloaded calendar file names (IV, XV) #25

--- TDP.Shared/Constants/File.cs
+++ TDP.Shared/Constants/File.cs
@@ -7,6 +7,12 @@ public static class File
     public const string Collections = "collections.csv";
 
     #region PDF Calendars
+    public const string GardonyIV = "Gardony_IV";
+    public const string GardonyIVEP = "Gardony_IV_EP";
+    public const string GardonyIVHH = "Gardony_IV_HH";
+    public const string GardonyXV = "Gardony_XV";
+    public const string GardonyXVEP = "Gardony_XV_EP";
+    public const string GardonyXVHH = "Gardony_XV_HH";
     public const string GardonyXVI = "Gardony_XVI";
     public const string GardonyXVIEP = "Gardony_XVI_EP";
     public const string GardonyXVIHH = "Gardony_XVI_HH";
