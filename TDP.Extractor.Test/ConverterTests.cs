7350c71e64d7cef772936f8b8ddbe5940c8a41e8
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-09-01 14:29:36 +0200

    Create ConverterTests class #27

---
+++ TDP.Extractor.Test/ConverterTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TDP.Extractor.Test;
+
+[TestClass]
+public class ConverterTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
