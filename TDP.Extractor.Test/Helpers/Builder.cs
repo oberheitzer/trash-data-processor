7e7a4371c86eec997790e5baea195b028bd25118
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-09-02 17:54:19 +0200

    Add helper class to test project #27

---
+++ TDP.Extractor.Test/Helpers/Builder.cs
@@ -0,0 +1,34 @@
+﻿using TDP.Domain.Model;
+
+namespace TDP.Extractor.Test;
+
+internal static class Builder
+{
+    public static List<Area> BuildAreas()
+        => [
+            new Area
+            {
+                Id = 1,
+                Name = "I",
+                SettlementId = 1,
+            },
+            new Area
+            {
+                Id = 4,
+                Name = "IV",
+                SettlementId = 1,
+            },
+            new Area
+            {
+                Id = 13,
+                Name = "XIII",
+                SettlementId = 1,
+            },
+            new Area
+            {
+                Id = 17,
+                Name = "Zártkert",
+                SettlementId = 1,
+            }
+        ];
+}
