bc7807e4725e7d60328308352998bc8b68241854
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-10-03 14:15:12 +0200

    Add DatabaseServiceTests class #28

---
+++ TDP.Http.Test/DatabaseServiceTests.cs
@@ -0,0 +1,25 @@
+namespace TDP.Http.Test;
+
+[TestClass]
+public class DatabaseServiceTests
+{
+    [TestMethod]
+    public void GetAreasAsync_Should_Work()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
