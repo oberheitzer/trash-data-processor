efb304506cf036499f42def36668be51737d2ddd
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-10-03 17:32:41 +0200

    Create test cases for DatabaseService #28

---
+++ TDP.Http.Test/Extensions/MoqExtension.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using Moq.Protected;
+
+namespace TDP.Http.Test;
+
+internal static class MoqExtension
+{
+    internal static Moq.Language.Flow.ISetup<HttpMessageHandler, Task<HttpResponseMessage>> SetupSendAsync(this Mock<HttpMessageHandler> handlerMock)
+    {
+        return handlerMock
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                methodOrPropertyName: "SendAsync", 
+                ItExpr.Is<HttpRequestMessage>(request => request.RequestUri != null && !string.IsNullOrEmpty(request.RequestUri.ToString())), 
+                ItExpr.IsAny<CancellationToken>()
+            );
+    }
+}
