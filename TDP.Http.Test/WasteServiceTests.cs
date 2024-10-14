e3e1ca3cce3bb81d10eee3c386ede9adc8fc44f8
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-10-14 23:36:44 +0200

    Add test cases to check DownloadAsync #28

--- TDP.Http.Test/WasteServiceTests.cs
+++ TDP.Http.Test/WasteServiceTests.cs
@@ -1,15 +1,65 @@
-﻿namespace TDP.Http.Test;
+﻿using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.Net;
+using FluentAssertions;
+using Moq;
+using TDP.Http.Services;
+
+namespace TDP.Http.Test;
 
 [TestClass]
 public class WasteServiceTests
 {
+    private readonly Mock<HttpMessageHandler> _handlerMock = new();
+    private readonly Mock<IFileSystem> _fileSystemMock = new();
+
     [TestMethod]
-    public void Test()
+    public async Task DownloadAsync_Should_Copy_The_Content_Of_The_Downloaded_File()
     {
         // Arrange
+        string content = "Test content";
+
+        var fileSystem = new MockFileSystem();
+
+        _ = _handlerMock
+            .SetupSendAsync()
+            .ReturnsAsync(Builder.BuildResponse(value: content));
+
+        Builder.BuildFileMocks(fileSystem, _fileSystemMock);
+
+        var service = new WasteService(
+            httpClient: Builder.CreateClient(handler: _handlerMock.Object),
+            fileSystem: _fileSystemMock.Object);
 
         // Act
+        await service.DownloadAsync();
 
         // Assert
+        MockFileData file = fileSystem.GetFile("test.pdf");
+        file.TextContents.Should().Contain(content);
+    }
+
+    [TestMethod]
+    public async Task DownloadAsync_Should_Not_Copy_Anything()
+    {
+        // Arrange
+        var fileSystem = new MockFileSystem();
+
+        _ = _handlerMock
+            .SetupSendAsync()
+            .ReturnsAsync(Builder.BuildResponse(value: null, code: HttpStatusCode.NotFound));
+
+        Builder.BuildFileMocks(fileSystem, _fileSystemMock);
+
+        var service = new WasteService(
+            httpClient: Builder.CreateClient(handler: _handlerMock.Object),
+            fileSystem: _fileSystemMock.Object);
+
+        // Act
+        await service.DownloadAsync();
+
+        // Assert
+        MockFileData file = fileSystem.GetFile("test.pdf");
+        file.TextContents.Should().BeEmpty();
     }
 }
