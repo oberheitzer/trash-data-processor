796bdda4c931ba40cfde884b18d2986a4d905f8e
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-10-14 23:35:22 +0200

    Update Builder #28

--- TDP.Http.Test/Helpers/Builder.cs
+++ TDP.Http.Test/Helpers/Builder.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.Net;
 using System.Text.Json;
+using Moq;
 using TDP.Domain.Model;
 
 namespace TDP.Http.Test;
@@ -16,13 +19,25 @@ internal static class Builder
         ];
     }
 
+    internal static void BuildFileMocks(MockFileSystem fileSystem, Mock<IFileSystem> fileSystemMock)
+    {
+         _ = fileSystemMock
+            .Setup(fileSystem => fileSystem.Directory.CreateDirectory(It.IsAny<string>()))
+            .Returns(fileSystem.Directory.CreateDirectory(path: "Test"));
+
+        _ = fileSystemMock
+            .Setup(fileSystem => fileSystem.File.Create(It.IsAny<string>()))
+            .Returns(fileSystem.File.Create(path: "test.pdf"));
+    }
+
     internal static HttpResponseMessage BuildResponse(object? value, HttpStatusCode code = HttpStatusCode.OK)
     {
-        return new HttpResponseMessage
+        var response = new HttpResponseMessage(statusCode: code);
+        if (value is not null)
         {
-            Content = new StringContent(JsonSerializer.Serialize(value: value)),
-            StatusCode = code
-        };
+            response.Content = new StringContent(JsonSerializer.Serialize(value: value));
+        }
+        return response;
     }
 
     internal static HttpClient CreateClient(HttpMessageHandler handler)
