1ee1cee8f832938c732fa0a536a8a6c69127cf43
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-10-14 23:36:14 +0200

    Add IFileSystem dependency and check Http status code #28

--- TDP.Http/Services/WasteService.cs
+++ TDP.Http/Services/WasteService.cs
@@ -1,4 +1,6 @@
-﻿using TDP.Http.Helpers;
+﻿using System.IO.Abstractions;
+using System.Net;
+using TDP.Http.Helpers;
 using TDP.Http.Interfaces;
 using TDP.Shared.Extensions;
 
@@ -7,22 +9,27 @@ namespace TDP.Http.Services;
 internal sealed class WasteService : IWasteService
 {
     private readonly HttpClient _httpClient;
+    private readonly IFileSystem _fileSystem;
 
-    public WasteService(HttpClient httpClient)
+    public WasteService(HttpClient httpClient, IFileSystem fileSystem)
     {
         _httpClient = httpClient;
+        _fileSystem = fileSystem;
     }
 
     public async Task DownloadAsync()
     {
-        var directory = Directory.CreateDirectory(path: DirectoryExtension.GetDirectoryPath(folderName: Shared.Constants.File.Calendars));
+        var directory = _fileSystem.Directory.CreateDirectory(path: DirectoryExtension.GetDirectoryPath(folderName: Shared.Constants.File.Calendars));
         
         foreach ((string fileName, string requestUri) in Shared.Constants.Uri.Areas)
         {
             var response = await _httpClient.GetAsync(requestUri: $"{Constant.Folder}{requestUri}");
-            using var file = File.Create(path: $@"{directory.FullName}/{fileName}.pdf");
-            var content = await response.Content.ReadAsStreamAsync();
-            await content.CopyToAsync(file);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                using var file = _fileSystem.File.Create(path: $@"{directory.FullName}/{fileName}.pdf");
+                var content = await response.Content.ReadAsStreamAsync();
+                await content.CopyToAsync(file);
+            }
         }
     }
 }
