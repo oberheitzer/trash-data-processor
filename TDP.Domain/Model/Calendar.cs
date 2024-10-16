6bed1224dedcc851cc42bdd8935cf96d298415b1
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-10-16 21:25:15 +0200

    Add Calendar domain class #62

---
+++ TDP.Domain/Model/Calendar.cs
@@ -0,0 +1,32 @@
+﻿namespace TDP.Domain.Model;
+
+/// <summary>
+/// Metadata of a calendar.
+/// </summary>
+public class Calendar
+{
+    /// <summary>
+    /// Unique identifier.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The specific part of the Uri where the calendar is located.
+    /// </summary>
+    public string Uri { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The given name of the downloaded file.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Foreign key.
+    /// </summary>
+    public int SettlementId { get; set; }
+    
+    /// <summary>
+    /// Navigation property.
+    /// </summary>
+    public Settlement Settlement { get; set; } = null!;
+}
