acfbcf2a80fb6488a998d39c94421dd6f37c2768
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-11-19 20:48:30 +0100

    delete commented put code #71

--- TDP.Extractor/Services/CalendarService.cs
+++ TDP.Extractor/Services/CalendarService.cs
@@ -68,7 +68,7 @@ internal sealed class CalendarService : ICalendarService
             // Don't write the header again.
             HasHeaderRecord = !_fileSystem.File.Exists(path: file),
         };
-        using StreamWriter writer = _fileSystem.File.AppendText(path: file); // new(path: file, append: true);
+        using StreamWriter writer = _fileSystem.File.AppendText(path: file);
         using CsvWriter csv = new(writer: writer, configuration: config);
         csv.Context.RegisterClassMap<CollectionMapper>();
         csv.WriteRecords(records: collections);
