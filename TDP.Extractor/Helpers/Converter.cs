9bc25f4d7b829a0a6af1ededdba5d6c971a74d1c
oberheitzer <bertalan.david.krisztian@gmail.com>
2025-01-05 18:36:10 +0100

    Hepler functions to convert month and day #90

--- TDP.Extractor/Helpers/Converter.cs
+++ TDP.Extractor/Helpers/Converter.cs
@@ -48,4 +48,33 @@ public static class Converter
         Constant.RecyclableWaste => Waste.Recyclable,
         _ => throw new ArgumentOutOfRangeException(nameof(code), $"Not expected code value: {code}"),
     };
+
+    public static DayOfWeek ToDayOfWeek(string day) => day switch
+    {
+        Week.Monday => DayOfWeek.Monday,
+        Week.Tuesday => DayOfWeek.Tuesday,
+        Week.Wednesday => DayOfWeek.Wednesday,
+        Week.Thursday => DayOfWeek.Thursday,
+        Week.Friday => DayOfWeek.Friday,
+        Week.Saturday => DayOfWeek.Saturday,
+        Week.Sunday => DayOfWeek.Sunday,
+        _ => throw new ArgumentOutOfRangeException($"Unexpected name of a day: {day}")
+    };
+
+    public static int ToMonthIndex(string month) => month switch
+    {
+        Month.January => 1,
+        Month.February => 2,
+        Month.March => 3,
+        Month.April => 4,
+        Month.May => 5,
+        Month.June => 6,
+        Month.July => 7,
+        Month.August => 8,
+        Month.September => 9,
+        Month.October => 10,
+        Month.November => 11,
+        Month.December => 12,
+        _ => throw new ArgumentOutOfRangeException($"Unexpected name of a month: {month}")
+    };
 }
