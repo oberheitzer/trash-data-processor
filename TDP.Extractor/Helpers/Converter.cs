c8c0c1827e90971753e6370ed2c19b4cd5869aae
oberheitzer <bertalan.david.krisztian@gmail.com>
2025-01-06 08:47:04 +0100

    Delete unused methods #90

--- TDP.Extractor/Helpers/Converter.cs
+++ TDP.Extractor/Helpers/Converter.cs
@@ -1,54 +1,7 @@
-﻿using TDP.Domain.Enum;
-using TDP.Domain.Model;
-
-namespace TDP.Extractor.Helpers;
+﻿namespace TDP.Extractor.Helpers;
 
 public static class Converter
 {
-    /// <summary>
-    /// Finds and returns the name of the area.
-    /// </summary>
-    /// <param name="line">Title line of the calendar.</param>
-    /// <returns>Name of area.</returns>
-    public static string ToArea(string line)
-    {
-        // TODO more settlement
-        string area = line.Split("Gárdony")[1].Trim();
-        int dotIndex = area.IndexOf('.');
-        if (dotIndex != -1)
-        {
-            return area.Remove(dotIndex);
-        }
-        return area;
-    }
-
-    /// <summary>
-    /// Creates and instance of the Collection class.
-    /// </summary>
-    /// <param name="year">Year of collection.</param>
-    /// <param name="month">Month of collection.</param>
-    /// <param name="day">Day of collection.</param>
-    /// <param name="code">The code of the type of waste.</param>
-    /// <param name="property">The type of the property.</param>
-    /// <returns>Created instance of Collection.</returns>
-    public static Collection ToCollection(int year, int month, int day, string code, int areaId, int id)
-    {
-        return new Collection
-        {
-            Date = new DateOnly(year: year, month: month, day: day),
-            Id = id,
-            Waste = ToWaste(code: code)
-        };
-    }
-
-    public static Waste ToWaste(string code) => code switch
-    {
-        Constant.SolidWaste => Waste.Solid,
-        Constant.OrganicWaste => Waste.Organic,
-        Constant.RecyclableWaste => Waste.Recyclable,
-        _ => throw new ArgumentOutOfRangeException(nameof(code), $"Not expected code value: {code}"),
-    };
-
     public static DayOfWeek ToDayOfWeek(string day) => day switch
     {
         Week.Monday => DayOfWeek.Monday,
