2774e5a9a937843a89d657b96db4f752412e6f83
oberheitzer <bertalan.david.krisztian@gmail.com>
2025-01-06 08:45:23 +0100

    Test cases of ToDayOfWeek and ToMonthIndex #90

--- TDP.Extractor.Test/ConverterTests.cs
+++ TDP.Extractor.Test/ConverterTests.cs
@@ -63,4 +63,56 @@ public class ConverterTests
         var exception = Assert.ThrowsException<ArgumentOutOfRangeException>(() => Converter.ToWaste(code: code));
         Assert.IsTrue(condition: exception.Message.Contains(expectedMessage));
     }
+
+    [DataTestMethod]
+    [DataRow("hétfő", DayOfWeek.Monday)]
+    [DataRow("szerda", DayOfWeek.Wednesday)]
+    [DataRow("vasárnap", DayOfWeek.Sunday)]
+    public void ToDayOfWeek_Should_Work(string day, DayOfWeek expected)
+    {
+        // Arrange & Act
+        DayOfWeek convertedValue = Converter.ToDayOfWeek(day: day);
+
+        // Assert
+        Assert.AreEqual(expected: expected, actual: convertedValue);
+    }
+
+    [DataTestMethod]
+    [DataRow("Hétfő")]
+    [DataRow("test")]
+    public void ToDayOfWeek_Should_Throw_Exception(string day)
+    {
+        // Arrange
+        string expectedMessage = $"Unexpected name of a day: {day}";
+
+        // Act & Assert
+        var exception = Assert.ThrowsException<ArgumentOutOfRangeException>(() => Converter.ToDayOfWeek(day: day));
+        Assert.IsTrue(condition: exception.Message.Contains(expectedMessage));
+    }
+
+    [DataTestMethod]
+    [DataRow("január", 1)]
+    [DataRow("május", 5)]
+    [DataRow("december", 12)]
+    public void ToMonthIndex_Should_Work(string month, int expected)
+    {
+        // Arrange & Act
+        int convertedValue = Converter.ToMonthIndex(month: month);
+
+        // Assert
+        Assert.AreEqual(expected: expected, actual: convertedValue);
+    }
+
+    [DataTestMethod]
+    [DataRow("Április")]
+    [DataRow("test")]
+    public void ToMonthIndex_Should_Throw_Exception(string month)
+    {
+        // Arrange
+        string expectedMessage = $"Unexpected name of a month: {month}";
+
+        // Act & Assert
+        var exception = Assert.ThrowsException<ArgumentOutOfRangeException>(() => Converter.ToMonthIndex(month: month));
+        Assert.IsTrue(condition: exception.Message.Contains(expectedMessage));
+    }
 }
