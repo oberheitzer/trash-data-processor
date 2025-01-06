ece172c8ac245e6f8fb3ff3559cfe01e51067185
oberheitzer <bertalan.david.krisztian@gmail.com>
2025-01-06 08:47:46 +0100

    Delete unnecessary test cases #90

--- TDP.Extractor.Test/ConverterTests.cs
+++ TDP.Extractor.Test/ConverterTests.cs
@@ -1,69 +1,10 @@
-﻿using TDP.Domain.Enum;
-using TDP.Domain.Model;
-using TDP.Extractor.Helpers;
+﻿using TDP.Extractor.Helpers;
 
 namespace TDP.Extractor.Test;
 
 [TestClass]
 public class ConverterTests
 {
-    [DataTestMethod]
-    [DataRow("2024. évi hulladéknaptár - Gárdony I.", "I")]
-    [DataRow("2024. évi hulladéknaptár - Gárdony Zártkert", "Zártkert")]
-    public void ToArea_Should_Work(string line, string expected)
-    {
-        // Arrange & Act
-        string area = Converter.ToArea(line: line);
-
-        // Assert
-        Assert.AreEqual(expected: expected, actual: area);
-    }
-
-    [TestMethod]
-    public void ToCollection_Should_Work()
-    {
-        // Arrange & Act
-        Collection collection = Converter.ToCollection(
-            year: 2024,
-            month: 6,
-            day: 15,
-            code: "V",
-            areaId: 1,
-            id: 1
-        );
-
-        // Assert
-        Assert.AreEqual(expected: new DateOnly(2024, 6, 15), actual: collection.Date);
-        Assert.AreEqual(expected: 1, actual: collection.Id);
-        // TODO Assert.AreEqual(expected: 1, actual: collection.AreaId);
-        Assert.AreEqual(expected: Waste.Solid, actual: collection.Waste);
-    }
-
-    [DataTestMethod]
-    [DataRow(Constant.SolidWaste, Waste.Solid)]
-    [DataRow(Constant.OrganicWaste, Waste.Organic)]
-    [DataRow(Constant.RecyclableWaste, Waste.Recyclable)]
-    public void ToWaste_Should_Work(string code, Waste expected)
-    {
-        // Arrange & Act
-        Waste convertedValue = Converter.ToWaste(code: code);
-
-        // Assert
-        Assert.AreEqual(expected: expected, actual: convertedValue);
-    }
-
-    [TestMethod]
-    public void ToWaste_Should_Throw_Exception()
-    {
-        // Arrange
-        string code = "Not a valid code";
-        string expectedMessage = $"Not expected code value: {code}";
-
-        // Act & Assert
-        var exception = Assert.ThrowsException<ArgumentOutOfRangeException>(() => Converter.ToWaste(code: code));
-        Assert.IsTrue(condition: exception.Message.Contains(expectedMessage));
-    }
-
     [DataTestMethod]
     [DataRow("hétfő", DayOfWeek.Monday)]
     [DataRow("szerda", DayOfWeek.Wednesday)]
